47106b213b7fc868236fe5134e40b38f70f44c9f
Mertt Doğanay <MERTLOL3@outlook.com>
2021-10-24 16:45:15 -0400

    QR code scanner implemented, user has to give access to the pc's camera and put a qr code infront of it for scanning

--- app/src/main/java/com/example/nontrivialapi/MainActivity.java
+++ app/src/main/java/com/example/nontrivialapi/MainActivity.java
@@ -1,14 +1,71 @@
 package com.example.nontrivialapi;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+//Ali Mert Doganay
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Variable assigning
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //intent integrator initializing
+                IntentIntegrator intentIntegrator = new IntentIntegrator(MainActivity.this);
+                intentIntegrator.setPrompt("Hello Professor, its me!!");
+                // beep voice
+                intentIntegrator.setBeepEnabled(true);
+                intentIntegrator.setOrientationLocked(true);
+                //capture activity set
+                intentIntegrator.setCaptureActivity(CodeScan.class);
+                //scan initiation
+                intentIntegrator.initiateScan();
+            }
+        });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //result initialing
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        if(result.getContents() != null){
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+            builder.setTitle("Scan Result is:");
+            builder.setMessage(result.getContents());
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+
+            });
+            builder.show();
+        }
+        else{
+            Toast.makeText(getApplicationContext(), "Scan Failed",Toast.LENGTH_SHORT).show();
+        }
     }
 }
